f8a7e33d0049b1ad7725863ee48db033cb39e7d3
freepsw <freepsw@gmail.com>
2016-10-01 11:19:26 +0900

    delete unnecessary files

--- src/main/scala/HelloScala.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * Created by P088466 on 2016-07-06.
-  */
-object HelloScala {
-  def main (args : Array[String]) : Unit = {
-    println("Hello Scala")
-  }
-}
--- src/main/scala/io/skiper/writer/RedisWriter.scala
+++
@@ -1,8 +0,0 @@
-package io.skiper.writer
-
-/**
-  * Created by P088466 on 2016-07-26.
-  */
-object RedisWriter {
-
-}
